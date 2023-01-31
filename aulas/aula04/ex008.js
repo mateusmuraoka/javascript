1a4ba57f0369bce07667518fc991881da355efcc
Mateus Muraoka Abreu <muraokaabreu1@gmail.com>
2023-01-31 11:00:19 -0300

    Condição

--- aulas/aula04/ex008.js
+++ aulas/aula04/ex008.js
@@ -1,1 +1,6 @@
-console.log('O console funcionou corretamente.')
+var vel = 12.5
+console.log(`A velocidade do seu carro ${vel} km/h.`)
+if (vel > 60){ //condição simples
+    console.log('Você ultrapassou a velocidade permitida,MUTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança.')
---
+++ aulas/aula04/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'França'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'BRASIL'){
+        console.log('VOcê é Brasileiro')
+}else{
+    console.log('Você é Estrangeiro')
+}
