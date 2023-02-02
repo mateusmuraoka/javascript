1adb25da83f921606234b1a67a2c07b1d3c827a8
Mateus Muraoka Abreu <muraokaabreu1@gmail.com>
2023-02-02 11:02:36 -0300

    Repetições e início de array

---
+++ aulas/aula06/ex014.js
@@ -0,0 +1,19 @@
+/*
+console.log('Tudo Bem!')
+console.log('Tudo Bem!')
+console.log('Tudo Bem!')
+console.log('Tudo Bem!')
+console.log('Tudo Bem!')
+console.log('Tudo Bem!')
+*/
+/*var c = 1
+while (c <= 6){
+    console.log('Tudo bem!')
+    c += 1
+}
+*/
+var c = 1
+do{
+    console.log(`passo ${c}`)
+    c = c + 1
+}while(c <= 6)
