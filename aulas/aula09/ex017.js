ba46d907fb7001d601da55f792551f8aec810af1
Mateus Muraoka Abreu <muraokaabreu1@gmail.com>
2023-02-06 11:14:14 -0300

    Funções

---
+++ aulas/aula09/ex017.js
@@ -0,0 +1,27 @@
+/*function parimpar(n){
+    if (n%2 == 0){
+        return 'par'
+    }else {
+        return 'impar'
+    }
+}
+let res = parimpar(232323)
+console.log(res)
+*/
+/*function soma(n1=0,n2=0){
+    return n1 + n2
+}
+let res = soma(35)
+console.log(res)
+*/
+/*var v = function(x){
+    return x*2
+}
+console.log(v(5))
+*/
+function fatorial(n){
+    let fat = 1
+    for (let c;c > 1;c--){
+        fat = fat + c
+    }
+}
