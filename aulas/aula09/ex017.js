b12d26217105a510d30a12d2ba22b8811e8e3206
Mateus Muraoka Abreu <muraokaabreu1@gmail.com>
2023-02-06 11:58:56 -0300

    Introdução a objetos

--- aulas/aula09/ex017.js
+++ aulas/aula09/ex017.js
@@ -21,7 +21,8 @@ console.log(v(5))
 */
 function fatorial(n){
     let fat = 1
-    for (let c;c > 1;c--){
-        fat = fat + c
+    for (let c = n;c > 1;c--){
+        fat = fat * c
     }
+    return fat
 }
