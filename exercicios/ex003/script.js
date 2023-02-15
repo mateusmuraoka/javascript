5bbe042cc1353d77b14a2f81fe67f11e4a94a0d7
Mateus Muraoka Abreu <muraokaabreu1@gmail.com>
2023-02-15 11:50:59 -0300

    ex003

--- exercicios/ex003/script.js
+++ exercicios/ex003/script.js
@@ -1,3 +1,20 @@
 function contar(){
-    console.log('OK')
+    var i = window.document.getElementById('i')
+    var f = window.document.getElementById('f')
+    var p = window.document.getElementById('p')
+    var res = window.document.getElementById('res')
+    if (i.value == '' || f.value == ''){
+        res.innerText = 'Impossível contar!'
+
+    }else if (p.value == ''){
+        window.alert('Passo inválido!.Considerando Passo com valor 1.')
+        p = 1
+    }else if (f > i){
+        for (var num = 0 , )
+    }
+
+    
+
+    
+    
 }
