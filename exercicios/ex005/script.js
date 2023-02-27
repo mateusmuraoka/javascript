0b3121807a451f4814cbc7810953260e31981c0b
Mateus Muraoka Abreu <muraokaabreu1@gmail.com>
2023-02-27 16:30:04 -0300

    ex005

--- exercicios/ex005/script.js
+++ exercicios/ex005/script.js
@@ -20,5 +20,37 @@ function adicionar(){
   
 }
 function finalizar(){
-    window.alert(dados)
+    
+    
+    let maior = 0
+    let menor = 0
+    let soma = 0
+    //Descobrindo o maior valor do array
+    for (let c = 0 ; c < dados.length; c++){
+        if (dados[c]> maior){
+            maior = dados[c]
+        }
+    }
+    //descobrindo o menor valor do array
+    for (let i =0 ; i < dados.length; i ++){
+        if (i ==0){
+            menor = dados[i]
+        }else{
+            if(dados[i] < menor){
+                menor = dados[i]
+            }
+        }
+    }
+    //Soma dos valores contidos no array
+    for(t = 0; t < dados.length; t++){
+        soma = soma + dados[t]
+    }
+    //Cálculo da média dos valores contidos no array
+    let media = soma / dados.length
+    res1.innerHTML = ''
+    res1.innerHTML += `<p>Ao todo, temos ${dados.length} números</p>`
+    res1.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+    res1.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+    res1.innerHTML += `<p>A soma dos valores digitados é ${soma}</p>`
+    res1.innerHTML += `<p>A média dos valores digitados é ${media}</p>`
 }
